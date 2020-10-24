3a26cad7be5d7d9c285e1c05952ac87c8f0ab968
Michael Ruocco <michael.ruocco@hotmail.com>
2020-10-24 22:07:19 +0100

    applies spotless and updates jacoco version

--- src/main/java/uk/co/mruoc/spring/filter/Regexs.java
+++ src/main/java/uk/co/mruoc/spring/filter/Regexs.java
@@ -3,7 +3,7 @@ package uk.co.mruoc.spring.filter;
 public class Regexs {
 
     public static final String UUID = "[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}";
-    
+
     private Regexs() {
         // utility class
     }
